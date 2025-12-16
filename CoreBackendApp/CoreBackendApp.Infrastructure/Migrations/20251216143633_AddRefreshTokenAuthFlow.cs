bb2bef87713759d3f47ced20579696ffb4f72b53
Zoran Trkulja <zoran87sk@gmail.com>
2025-12-16 15:44:55 +0100

    added migration

---
+++ CoreBackendApp/CoreBackendApp.Infrastructure/Migrations/20251216143633_AddRefreshTokenAuthFlow.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreBackendApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenAuthFlow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_RefreshTokens_RefreshTokenId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_RefreshTokenId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsRevoked",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenId",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "Token",
+                table: "RefreshTokens",
+                newName: "TokenHash");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_TokenHash");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByIp",
+                table: "RefreshTokens",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReplacedByTokenHash",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByIp",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "ReplacedByTokenHash",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "TokenHash",
+                table: "RefreshTokens",
+                newName: "Token");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_TokenHash",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_Token");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRevoked",
+                table: "RefreshTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RefreshTokenId",
+                table: "RefreshTokens",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_RefreshTokenId",
+                table: "RefreshTokens",
+                column: "RefreshTokenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_RefreshTokens_RefreshTokenId",
+                table: "RefreshTokens",
+                column: "RefreshTokenId",
+                principalTable: "RefreshTokens",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
