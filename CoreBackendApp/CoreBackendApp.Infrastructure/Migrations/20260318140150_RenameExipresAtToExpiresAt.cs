017c0697680b3f09b5062f4beacda7c087e6ffd1
Zoran Trkulja <zoran87sk@gmail.com>
2026-03-18 15:04:57 +0100

    chore: add migration to rename ExipresAt to ExpiresAt

---
+++ CoreBackendApp/CoreBackendApp.Infrastructure/Migrations/20260318140150_RenameExipresAtToExpiresAt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreBackendApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameExipresAtToExpiresAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExipresAt",
+                table: "RefreshTokens",
+                newName: "ExpiresAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpiresAt",
+                table: "RefreshTokens",
+                newName: "ExipresAt");
+        }
+    }
+}
